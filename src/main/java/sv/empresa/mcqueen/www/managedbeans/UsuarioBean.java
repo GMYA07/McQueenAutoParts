7e7d317ca85b1852dcb335c1e722a082ccf2798f
YAMG_7 <89718678+GMYA07@users.noreply.github.com>
2023-10-26 17:34:38 -0600

    Empezare a realizar el registro de usuarios

--- src/main/java/sv/empresa/mcqueen/www/managedbeans/UsuarioBean.java
+++ src/main/java/sv/empresa/mcqueen/www/managedbeans/UsuarioBean.java
@@ -2,8 +2,31 @@ package sv.empresa.mcqueen.www.managedbeans;
 
 import jakarta.faces.bean.ManagedBean;
 import jakarta.faces.bean.RequestScoped;
+import sv.empresa.mcqueen.www.entities.UsuarioEntity;
+import sv.empresa.mcqueen.www.models.UsuariosModel;
+
+import java.util.List;
 
 @ManagedBean
 @RequestScoped
 public class UsuarioBean {
+
+    UsuarioEntity usuario ;
+    UsuariosModel modeloUsuario = new UsuariosModel();
+    List<UsuarioEntity> listaUsuarios;
+    public UsuarioBean(){usuario = new UsuarioEntity();}
+
+
+    //GETTER AND SETTER
+    public List<UsuarioEntity> getListaUsuarios() {
+        return modeloUsuario.listarUsuarios();
+    }
+
+    public UsuarioEntity getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(UsuarioEntity usuario) {
+        this.usuario = usuario;
+    }
 }
--- src/main/java/sv/empresa/mcqueen/www/models/UsuariosModel.java
+++ src/main/java/sv/empresa/mcqueen/www/models/UsuariosModel.java
@@ -1,4 +1,14 @@
 package sv.empresa.mcqueen.www.models;
 
+import sv.empresa.mcqueen.www.entities.UsuarioEntity;
+
+import java.util.List;
+
 public class UsuariosModel {
+
+    public List<UsuarioEntity> listarUsuarios(){
+        List<UsuarioEntity> lista = null;
+
+        return lista;
+    }
 }

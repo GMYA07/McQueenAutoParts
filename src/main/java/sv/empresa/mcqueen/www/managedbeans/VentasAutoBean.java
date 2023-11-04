c9cba7e432cde638c6da64ed62e458248da5421b
YAMG_7 <89718678+GMYA07@users.noreply.github.com>
2023-11-04 16:53:44 -0600

    Se empezo a realizar el proceso de venta de autos de usuarios

--- src/main/java/sv/empresa/mcqueen/www/managedbeans/VentasAutoBean.java
+++ src/main/java/sv/empresa/mcqueen/www/managedbeans/VentasAutoBean.java
@@ -1,25 +1,57 @@
 package sv.empresa.mcqueen.www.managedbeans;
 
+import jakarta.faces.application.FacesMessage;
 import jakarta.faces.bean.ManagedBean;
 import jakarta.faces.bean.RequestScoped;
+import jakarta.faces.context.FacesContext;
 import sv.empresa.mcqueen.www.entities.AutomovilesEntity;
 import sv.empresa.mcqueen.www.entities.VentasautoEntity;
 import sv.empresa.mcqueen.www.models.AutomovilesModel;
+import sv.empresa.mcqueen.www.models.UsuariosModel;
 import sv.empresa.mcqueen.www.models.VentasAutoModel;
+import sv.empresa.mcqueen.www.utils.JsfUtil;
 
 import java.util.List;
+import java.util.Random;
 
 @ManagedBean
 @RequestScoped
 public class VentasAutoBean {
     private VentasautoEntity ventaAuto;
     private AutomovilesEntity automovil;
-    private VentasAutoModel modeloVenta;
-    private AutomovilesModel modeloAutomovil;
+    private VentasAutoModel modeloVenta = new VentasAutoModel();
+    private AutomovilesModel modeloAutomovil = new AutomovilesModel();
+    private UsuariosModel modeloUsuario = new UsuariosModel();
 
     private List<VentasautoEntity> listaVentas;
 
-    public VentasAutoBean(){ventaAuto = new VentasautoEntity();}
+    public VentasAutoBean(){ventaAuto = new VentasautoEntity(); automovil = new AutomovilesEntity();}
+
+    public void registrarVentaUsuario(){
+        //settearemos los demas campos que requiere la venta
+        ventaAuto.setUsuarioByIdCliente(modeloUsuario.obtenerUsuario(JsfUtil.getRequest().getParameter("idComprador")));
+        ventaAuto.setAutomovilesByIdCarro(modeloAutomovil.obtenerAutomovil(JsfUtil.getRequest().getParameter("idAuto")));
+        ventaAuto.setEstado(12);
+        ventaAuto.setPrecio(123);
+        //crearemos el id para la venta
+        crearIDVentaAuto(1);
+        //registraremos la venta
+        if (modeloVenta.registrarVentaAutomovilUsuario(ventaAuto) == 1){
+            FacesContext.getCurrentInstance().addMessage("successMessage", new FacesMessage(FacesMessage.SEVERITY_INFO, "Ingresado la venta Exitosamente", "Registrado"));
+        }else {
+            FacesContext.getCurrentInstance().addMessage("successMessage", new FacesMessage(FacesMessage.SEVERITY_INFO, "Error al enviar la venta ", "Error"));
+        }
+    }
+
+    public void crearIDVentaAuto(int tipo){
+        Random rand = new Random();
+        int numeroAleatorio = rand.nextInt(900) + 100;
+        if (tipo == 1){
+            ventaAuto.setIdVenta("VTU"+numeroAleatorio);
+        }else {
+            ventaAuto.setIdVenta("VTA"+numeroAleatorio);
+        }
+    }
 
     public void settearFormVentas(String idAuto){
         automovil = modeloAutomovil.obtenerAutomovil(idAuto);
--- src/main/java/sv/empresa/mcqueen/www/models/VentasAutoModel.java
+++ src/main/java/sv/empresa/mcqueen/www/models/VentasAutoModel.java
@@ -1,6 +1,9 @@
 package sv.empresa.mcqueen.www.models;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 import sv.empresa.mcqueen.www.entities.VentasautoEntity;
+import sv.empresa.mcqueen.www.utils.JpaUtil;
 
 import java.util.List;
 
@@ -8,5 +11,18 @@ public class VentasAutoModel {
     public List<VentasautoEntity>listarVentas(){
         List<VentasautoEntity> lista = null;
         return lista;
+    }
+    public int registrarVentaAutomovilUsuario(VentasautoEntity newVenta){
+        EntityManager eM = JpaUtil.getEntityManager();
+        EntityTransaction tran = eM.getTransaction();
+        try {
+            tran.begin();
+            eM.persist(newVenta);
+            tran.commit();
+            return 1;
+        }catch (Exception e){
+            eM.close();
+            return 0;
+        }
     }
 }

20ea437d7f603b577da4ac8d8ff488e3852ff9a3
YAMG_7 <89718678+GMYA07@users.noreply.github.com>
2023-11-03 22:32:29 -0600

    Haciendo cositas

--- src/main/java/sv/empresa/mcqueen/www/managedbeans/VentasAutoBean.java
+++ src/main/java/sv/empresa/mcqueen/www/managedbeans/VentasAutoBean.java
@@ -2,8 +2,48 @@ package sv.empresa.mcqueen.www.managedbeans;
 
 import jakarta.faces.bean.ManagedBean;
 import jakarta.faces.bean.RequestScoped;
+import sv.empresa.mcqueen.www.entities.AutomovilesEntity;
+import sv.empresa.mcqueen.www.entities.VentasautoEntity;
+import sv.empresa.mcqueen.www.models.AutomovilesModel;
+import sv.empresa.mcqueen.www.models.VentasAutoModel;
+
+import java.util.List;
 
 @ManagedBean
 @RequestScoped
 public class VentasAutoBean {
+    private VentasautoEntity ventaAuto;
+    private AutomovilesEntity automovil;
+    private VentasAutoModel modeloVenta;
+    private AutomovilesModel modeloAutomovil;
+
+    private List<VentasautoEntity> listaVentas;
+
+    public VentasAutoBean(){ventaAuto = new VentasautoEntity();}
+
+    public void settearFormVentas(String idAuto){
+        automovil = modeloAutomovil.obtenerAutomovil(idAuto);
+    }
+
+
+    //GETTER Y SETTER
+    public List<VentasautoEntity> getListaVentas() {
+        return modeloVenta.listarVentas();
+    }
+
+    public VentasautoEntity getVentaAuto() {
+        return ventaAuto;
+    }
+
+    public void setVentaAuto(VentasautoEntity ventaAuto) {
+        this.ventaAuto = ventaAuto;
+    }
+
+    public AutomovilesEntity getAutomovil() {
+        return automovil;
+    }
+
+    public void setAutomovil(AutomovilesEntity automovil) {
+        this.automovil = automovil;
+    }
 }
--- src/main/java/sv/empresa/mcqueen/www/models/VentasAutoModel.java
+++ src/main/java/sv/empresa/mcqueen/www/models/VentasAutoModel.java
@@ -1,4 +1,12 @@
 package sv.empresa.mcqueen.www.models;
 
+import sv.empresa.mcqueen.www.entities.VentasautoEntity;
+
+import java.util.List;
+
 public class VentasAutoModel {
+    public List<VentasautoEntity>listarVentas(){
+        List<VentasautoEntity> lista = null;
+        return lista;
+    }
 }
